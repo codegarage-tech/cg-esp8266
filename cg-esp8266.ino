42b98b6895977d3da393980d1347b5d5f9e93a90
Md. Rashadul Alam <rashed.droid@gmail.com>
2019-01-10 01:38:54 +0600

    cg-esp8266: updated ino

--- cg-esp8266.ino
+++ cg-esp8266.ino
@@ -1,136 +1,31 @@
-/*    This is a bare bone library for communicating with SIM800
- *    It's barebone in that - it only provides basic functunaitlites while still 
- *    maintaining strong performance and being memory friendly.
- *    It currently supports GSM(sending and reading SMS), 
- *    GPRS connectivity(sending and receiving TCP) with Time and Location
- *     
- *    This library is writing by  Ayo Ayibiowu.
- *    charlesayibiowu@hotmail.com
- *    Designed to work with the GSM Sim800l module,
- *    and possibily other SIMxxx modules(not tested with others)
- *  
- *    This library use AltSoftSerial, and the pins has already be defined
- *    be sure that GND is attached to arduino too. 
- *    The RESET_PIN is not used for this Library, you can leave it un-connected
- *    
- *    To Enable Debugging - Go to <BareBoneSim800.h file and change the
- *    #define DEBUG 0 to #define DEBUG 1
- *         
- *    PINOUT: 
- *        _____________________________
- *       |  ARDUINO UNO >>>   SIM800L  |
- *        -----------------------------
- *            GND      >>>   GND
- *        RX  8       >>>   TX    
- *        TX  9       >>>   RX
- *      
- *                 
- *   POWER SOURCE 4.2V (MAX) >>> VCC
- *
- *        Created on: Oct 24, 2017
- *        Author: Ayo Ayibiowu
- *        Email: charlesayibiowu@hotmail.com
- *        Version: v1.0
- *        
- *
-*/
+// PHYSICAL STUFF:
+// Arduino pin 2 (RX) to ESP8266 TX
+// Arduino pin 3 (RX) to Voltage Divider, then to ESP8266 RX
+// Connect GND from the Arduino to GMD on the ESP8266
+// Connect Arduino 3.3V to VCC
+// Pull ESP8266 CH_PD HIGH (also connect to 3.3V rail)
 
-/*
- * ESP8266 NodeMCU LED Control over WiFi Demo
- *
- * https://circuits4you.com
- */
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
- 
-//ESP Web Server Library to host a web page
-#include <ESP8266WebServer.h>
- 
-//---------------------------------------------------------------
-//Our HTML webpage contents in program memory
-const char MAIN_page[] PROGMEM = R"=====(
-<!DOCTYPE html>
-<html>
-<body>
-<center>
-<h1>WiFi LED on off demo: 1</h1><br>
-Ciclk to turn <a href="ledOn">LED ON</a><br>
-Ciclk to turn <a href="ledOff">LED OFF</a><br>
-<hr>
-<a href="https://circuits4you.com">circuits4you.com</a>
-</center>
- 
-</body>
-</html>
-)=====";
-//---------------------------------------------------------------
-//On board LED Connected to GPIO2
-#define LED 2  
- 
-//SSID and Password of your WiFi router
-const char* ssid = "circuits4you.com";
-const char* password = "123456789";
- 
-//Declare a global object variable from the ESP8266WebServer class.
-ESP8266WebServer server(80); //Server on port 80
- 
-//===============================================================
-// This routine is executed when you open its IP in browser
-//===============================================================
-void handleRoot() {
- Serial.println("You called root page");
- String s = MAIN_page; //Read HTML contents
- server.send(200, "text/html", s); //Send web page
-}
- 
-void handleLEDon() { 
- Serial.println("LED on page");
- digitalWrite(LED,LOW); //LED is connected in reverse
- server.send(200, "text/html", "LED is ON"); //Send ADC value only to client ajax request
-}
- 
-void handleLEDoff() { 
- Serial.println("LED off page");
- digitalWrite(LED,HIGH); //LED off
- server.send(200, "text/html", "LED is OFF"); //Send ADC value only to client ajax request
-}
-//==============================================================
-//                  SETUP
-//==============================================================
-void setup(void){
+// Commands entered in the serial monitor are relayed to the ESP8266
+
+#include <SoftwareSerial.h>
+SoftwareSerial ESPserial (2,3); //RX|TX
+
+
+void setup() {
+  // put your setup code here, to run once:
+
   Serial.begin(115200);
-  
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
-  Serial.println("");
- 
-  //Onboard LED port Direction output
-  pinMode(LED,OUTPUT); 
-  //Power on LED state off
-  digitalWrite(LED,HIGH);
-  
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
- 
-  //If connection successful show IP address in serial monitor
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
- 
-  server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
-  server.on("/ledOn", handleLEDon); //as Per  <a href="ledOn">, Subroutine to be called
-  server.on("/ledOff", handleLEDoff);
- 
-  server.begin();                  //Start server
-  Serial.println("HTTP server started");
+  ESPserial.begin(115200);
 }
-//==============================================================
-//                     LOOP
-//==============================================================
-void loop(void){
-  server.handleClient();          //Handle client requests
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (ESPserial.available()){
+    Serial.write(ESPserial.read());
+    }
+
+    if (Serial.available()){
+    ESPserial.write(Serial.read());
+    }
 }
