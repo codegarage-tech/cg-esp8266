77bf93d6081bb8beb75af33d6a9e85c3d35dc6a8
Md. Rashadul Alam <rashed.droid@gmail.com>
2019-01-30 00:25:52 +0600

    cg-esp8266: updated wifi client code

--- example/http-test/esp8266_led/esp8266_led.ino
+++ example/http-test/esp8266_led/esp8266_led.ino
@@ -5,18 +5,15 @@ const char* ssid     = "Dicosta";
 const char* password = "ramo011911";
 const char* host = "iot-cuddle.000webhostapp.com";
 
-String url;
-int count = 0;
+int gpio = 1;
 
 void setup() {
   Serial.begin(115200);
   delay(100);
   pinMode(0, OUTPUT);
   pinMode(2, OUTPUT);
-//  pinMode(D3, OUTPUT);
   digitalWrite(0, LOW);
   digitalWrite(2, LOW);
-//  digitalWrite(D3, LOW);
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
@@ -38,61 +35,52 @@ void setup() {
   Serial.println(WiFi.gatewayIP());
   Serial.print("MAC: ");
   Serial.println(WiFi.macAddress());
-  digitalWrite(0, HIGH);
-  delay(500);
-  digitalWrite(0, LOW);
-  delay(500);
 }
 
 void loop() {
-
-  Serial.print("connecting to ");
-  Serial.println(host);
-
   WiFiClient client;
   const int httpPort = 80;
+  Serial.print("connecting to ");
+  Serial.println(host);
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
+    Serial.println("wait 5 sec...");
+    delay(5000);
     return;
   }
-
-  if (count == 0){
+  String url = "";
+  if (gpio == 1){
+    Serial.println("Preparing GPIO 0 url");
     url = "/iot/api/led/specific.php?id=1";
-    count = count + 1;
-    Serial.println("Here1");
-  } else if(count == 1){
+  } else if(gpio == 2){
+    Serial.println("Preparing GPIO 2 url");
     url = "/iot/api/led/specific.php?id=2";
-    count = count + 1;
-    Serial.println("Here2");
   }
-//  else if(count == 2){
-//    url = "/iot/api/led/specific.php?id=3";
-//    count = count + 1;
-//    Serial.println("Here3");
-//  }
   Serial.print("Requesting URL: ");
-  Serial.println(url);
+  Serial.println("https://iot-cuddle.000webhostapp.com"+url);
   
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+  client.print(String("GET ") +
+                url +
+               " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" + 
                "Connection: close\r\n\r\n");
   delay(500);
   String section="header";
   while(client.available()){
     String line = client.readStringUntil('\r');
-    //Serial.print(line);
+    Serial.print("line: "+line);
     // we’ll parse the HTML body here
     if (section=="header") { // headers..
-
       if (line=="\n") { // skips the empty space at the beginning
         section="json";
       }
     } else if (section=="json") {  // print the good stuff
       section="ignore";
-      String result = line.substring(1);
-
       // Parse JSON
+      String result = line.substring(1);
+      Serial.println("result: "+result);
       int size = result.length() + 1;
+      Serial.println("size: "+size);
       char json[size];
       result.toCharArray(json, size);
       StaticJsonBuffer<200> jsonBuffer;
@@ -101,44 +89,36 @@ void loop() {
         Serial.println("parseObject() failed");
         return;
       }
-      String led = json_parsed["led"][0]["status"];
-
-      if(count == 1){
-        if(led == "on"){
+      String gpioId = json_parsed["led"][0]["id"];
+      String gpioStatus = json_parsed["led"][0]["status"];
+      if(gpioId== "1"){
+        if(gpioStatus == "on"){
           digitalWrite(0, HIGH);
           delay(100);
-          Serial.println("GPIO0 is On..!");
-        } else if(led == "off"){
+          Serial.println("GPIO 0 is On..!");
+        } else if(gpioStatus == "off"){
           digitalWrite(0, LOW);
           delay(100);
-          Serial.println("GPIO0 is Off..!");
+          Serial.println("GPIO 0 is Off..!");
         }
-      } else if(count == 2){
-        if(led == "on"){
+        gpio = 2;
+      } else if(gpioId == "2"){
+        if(gpioStatus == "on"){
           digitalWrite(2, HIGH);
-          Serial.println("GPIO2 is On..!");
-        } else if(led == "off"){
+          Serial.println("GPIO 2 is On..!");
+        } else if(gpioStatus == "off"){
           digitalWrite(2, LOW);
-          Serial.println("GPIO2 is Off..!");
+          Serial.println("GPIO 2 is Off..!");
         }
+        gpio = 1;
       }
-//      else if(count == 3){
-//        if(led == "on"){
-//          digitalWrite(D3, HIGH);
-//          Serial.println("D3 is On..!");
-//        } else if(led == "off"){
-//          digitalWrite(D3, LOW);
-//          Serial.println("D3 is Off..!");
-//        }
-//        count = 0;
-//      }
-
-      if (count == 2){
-          count = 0;
+      Serial.println("Next gpio id: "+ gpio);
       }
-    }
   }
-  Serial.println();
+  
   Serial.println("closing connection");
-  delay(3000);
+  client.stop();
+
+  Serial.println("wait 5 sec...");
+  delay(5000);
 }
