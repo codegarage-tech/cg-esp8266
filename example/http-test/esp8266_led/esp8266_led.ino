55ea76ffed791886cd3f3d241924f492b189b0cc
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2019-01-29 10:43:32 +0600

    cg-esp8266: updated led ino file

--- example/http-test/esp8266_led/esp8266_led.ino
+++ example/http-test/esp8266_led/esp8266_led.ino
@@ -60,13 +60,11 @@ void loop() {
     url = "/iot/api/led/specific.php?id=1";
     count = count + 1;
     Serial.println("Here1");
-  }
-  else if (count == 1){
+  } else if(count == 1){
     url = "/iot/api/led/specific.php?id=2";
     count = count + 1;
     Serial.println("Here2");
-  }
-  else if (count == 2){
+  } else if(count == 2){
     url = "/iot/api/led/specific.php?id=3";
     count = count + 1;
     Serial.println("Here3");
@@ -88,8 +86,7 @@ void loop() {
       if (line=="\n") { // skips the empty space at the beginning
         section="json";
       }
-    }
-    else if (section=="json") {  // print the good stuff
+    } else if (section=="json") {  // print the good stuff
       section="ignore";
       String result = line.substring(1);
 
@@ -99,8 +96,7 @@ void loop() {
       result.toCharArray(json, size);
       StaticJsonBuffer<200> jsonBuffer;
       JsonObject& json_parsed = jsonBuffer.parseObject(json);
-      if (!json_parsed.success())
-      {
+      if (!json_parsed.success()) {
         Serial.println("parseObject() failed");
         return;
       }
@@ -111,38 +107,33 @@ void loop() {
           digitalWrite(D1, 1);
           delay(100);
           Serial.println("D1 is On..!");
-        }
-        else if(led == "off"){
+        } else if(led == "off"){
           digitalWrite(D1, 0);
           delay(100);
           Serial.println("D1 is Off..!");
         }
-      }
-      else if(count == 2){
+      } else if(count == 2){
         if(led == "on"){
           digitalWrite(D2, 1);
           Serial.println("D2 is On..!");
-        }
-        else if(led == "off"){
+        } else if(led == "off"){
           digitalWrite(D2, 0);
           Serial.println("D2 is Off..!");
         }
-      }
-      else if(count == 3){
+      } else if(count == 3){
         if(led == "on"){
           digitalWrite(D3, 1);
           Serial.println("D3 is On..!");
-        }
-        else if(led == "off"){
+        } else if(led == "off"){
           digitalWrite(D3, 0);
           Serial.println("D3 is Off..!");
         }
         count = 0;
       }
 
-      if (count == 3)
-        count = 0;
-
+      if (count == 3){
+          count = 0;
+      }
     }
   }
   Serial.println();
