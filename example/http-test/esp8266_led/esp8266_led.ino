12d35c24023936d4ea8e235ee82c7f915635598e
Md. Rashadul Alam <rashed.droid@gmail.com>
2019-04-24 01:42:45 +0600

    cg-esp8266: added wifi signal state with led

--- example/http-test/esp8266_led/esp8266_led.ino
+++ example/http-test/esp8266_led/esp8266_led.ino
@@ -14,13 +14,12 @@ const char* ssid     = "Dicosta";
 const char* password = "ramo011911";
 //const char* host = "iot-cuddle.000webhostapp.com";
 //int gpio = 1;
-int GPIO0 = 0, GPIO2 = 2;
-//TX = 1, RX = 3;
+int GPIO0 = 0, GPIO2 = 2, TX = 1, RX = 3;
 
 // Declare an RGBLED instanced named rgbLed
 // Red, Green and Blue LED legs are connected to PWM pins 11,9 & 6 respectively
 // In this example, we have a COMMON_ANODE LED, use COMMON_CATHODE otherwise
-//RGBLED rgbLed(TX,GPIO2,RX,COMMON_ANODE);
+//RGBLED rgbLed(TX, GPIO2, RX, COMMON_ANODE);
 
 void connectToWifi() {
   delay(1000);
@@ -75,35 +74,35 @@ void setup() {
   // Setup different pins
   pinMode(GPIO0, OUTPUT);
   //////////////////////
-//pinMode(TX, OUTPUT);
-pinMode(GPIO2, OUTPUT);
-//pinMode(RX, OUTPUT);
-///////////////////////
+  pinMode(TX, OUTPUT);
+  pinMode(GPIO2, OUTPUT);
+  pinMode(RX, OUTPUT);
+  ///////////////////////
   digitalWrite(GPIO0, HIGH);
-  /////////////////////
-//  analogWrite(TX, 1023);
-  analogWrite(GPIO2, 1023);
-//  analogWrite(RX, 1023);
+  ///////////////////
+    analogWrite(TX, 1023);
+    analogWrite(GPIO2, 1023);
+    analogWrite(RX, 1023);
   /////////////////////////
   Serial.println();
 
-//   Connect to wifi
+  //   Connect to wifi
   connectToWifi();
-} 
+}
 
 // the loop function runs over and over again forever
 void loopBlink() {
   delay(5000);
   digitalWrite(GPIO0, LOW);
   Serial.print("GPIO0 pin low\n");
-//  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  //  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
   // but actually the LED is on; this is because
   // it is active low on the ESP-01)
   delay(5000);                      // Wait for a second
   digitalWrite(GPIO0, HIGH);
   Serial.print("GPIO0 pin high\n");
-//  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-//  delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
+  //  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  //  delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
 }
 
 void loop() {
@@ -113,13 +112,13 @@ void loop() {
     HTTPClient http;
 
     String url = "";
-//    if (gpio == 1) {
-      Serial.println("Preparing GPIO 0 url");
-      url = "http://iot-cuddle.000webhostapp.com/iot/api/led/specific.php?id=1";
-//    } else if (gpio == 2) {
-//      Serial.println("Preparing GPIO 2 url");
-//      url = "https://iot-cuddle.000webhostapp.com/iot/api/led/specific.php?id=2";
-//    }
+    //    if (gpio == 1) {
+    Serial.println("Preparing GPIO 0 url");
+    url = "http://iot-cuddle.000webhostapp.com/iot/api/led/specific.php?id=1";
+    //    } else if (gpio == 2) {
+    //      Serial.println("Preparing GPIO 2 url");
+    //      url = "https://iot-cuddle.000webhostapp.com/iot/api/led/specific.php?id=2";
+    //    }
     Serial.print("Requesting URL: ");
     Serial.println(url);
 
@@ -156,28 +155,28 @@ void loop() {
           Serial.println("gpioStatus: " + gpioStatus);
 
           //do further task
-//          if (gpioId == "1") {
-            if (gpioStatus == "on") {
-              digitalWrite(GPIO0, LOW);
-              delay(100);
-              Serial.println("GPIO 0 is On..!");
-            } else if (gpioStatus == "off") {
-              digitalWrite(GPIO0, HIGH);
-              delay(100);
-              Serial.println("GPIO 0 is Off..!");
-            }
-//            gpio = 2;
-//          } else if (gpioId == "2") {
-//            if (gpioStatus == "on") {
-//              digitalWrite(2, LOW);
-//              Serial.println("GPIO 2 is On..!");
-//            } else if (gpioStatus == "off") {
-//              digitalWrite(2, HIGH);
-//              Serial.println("GPIO 2 is Off..!");
-//            }
-//            gpio = 1;
-//          }
-//          Serial.println("Next gpio id: " + gpio);
+          //          if (gpioId == "1") {
+          if (gpioStatus == "on") {
+            digitalWrite(GPIO0, LOW);
+            delay(100);
+            Serial.println("GPIO 0 is On..!");
+          } else if (gpioStatus == "off") {
+            digitalWrite(GPIO0, HIGH);
+            delay(100);
+            Serial.println("GPIO 0 is Off..!");
+          }
+          //            gpio = 2;
+          //          } else if (gpioId == "2") {
+          //            if (gpioStatus == "on") {
+          //              digitalWrite(2, LOW);
+          //              Serial.println("GPIO 2 is On..!");
+          //            } else if (gpioStatus == "off") {
+          //              digitalWrite(2, HIGH);
+          //              Serial.println("GPIO 2 is Off..!");
+          //            }
+          //            gpio = 1;
+          //          }
+          //          Serial.println("Next gpio id: " + gpio);
         } else {
           Serial.printf("[HTTP] GET... got null or empty response");
         }
@@ -188,35 +187,55 @@ void loop() {
 
     http.end();
   }
-//////////////////////////////////////////
-//  //Control wifi led signal
-//   long rssi = WiFi.RSSI();
-//   Serial.println("rssi: " + rssi);
-//   int bars = getBarsSignal(rssi);
-//   Serial.println("bars: " + bars);
-//   setWifiSignlaLed(bars);
+  //////////////////////////////////////////
+  //  //Control wifi led signal
+  //   long rssi = WiFi.RSSI();
+  //   Serial.println("rssi: " + rssi);
+  //   int bars = getBarsSignal(rssi);
+  //   Serial.println("bars: " + bars);
+  //   setWifiSignlaLed(bars);
 
-//static int previousQuality = -1;
-//  int quality = getQuality();
-//  if (quality != previousQuality) {  // If the quality changed since last print, print new quality and RSSI
-//    if (quality != -1){
-//      Serial.printf("WiFi Quality:\t%d\%\tRSSI:\t%d dBm\r\n", quality, WiFi.RSSI());
-      int bars = getBarsSignal(WiFi.RSSI());
-//      Serial.printf("bars: \t%d\r\n" , bars);
-      if((bars == 4)||(bars == 5)){
-        Serial.println("Wifi Signal is good: 4 or 5");
-      } else if(bars == 3){
-        Serial.println("Wifi Signal is medium: 3");
-       }else if((bars == 1)||(bars == 2)){
-        Serial.println("Wifi Signal is low: 1 or 2");
-        }else if(bars == 0){
-        Serial.println("Wifi Signal is not available: 0");
-         }
-//    }
-//    previousQuality = quality;
-//  }
+  //static int previousQuality = -1;
+  //  int quality = getQuality();
+  //  if (quality != previousQuality) {  // If the quality changed since last print, print new quality and RSSI
+  //    if (quality != -1){
+  //      Serial.printf("WiFi Quality:\t%d\%\tRSSI:\t%d dBm\r\n", quality, WiFi.RSSI());
+  int bars = getBarsSignal(WiFi.RSSI());
+  //      Serial.printf("bars: \t%d\r\n" , bars);
+  if ((bars == 4) || (bars == 5)) {
+    Serial.println("Wifi Signal is good: 4 or 5");
+    //Set the RGBLED to show GREEN only
+//    rgbLed.writeRGB(0, 255, 0);
+    analogWrite(GPIO2, 255);
+    analogWrite(RX,1023);
+    analogWrite(TX,1023);
+  } else if (bars == 3) {
+    Serial.println("Wifi Signal is medium: 3");
+    //Set the RGBLED to show BLUE only
+//    rgbLed.writeRGB(0, 0, 255);
+    analogWrite(GPIO2, 1023);
+    analogWrite(RX,1023);
+    analogWrite(TX,255);
+  } else if ((bars == 1) || (bars == 2)) {
+    Serial.println("Wifi Signal is low: 1 or 2");
+    //Set the RGBLED to show YELLOW (RED & GREEN) only
+//    rgbLed.writeRGB(255, 255, 0);
+    analogWrite(GPIO2, 1023);
+    analogWrite(RX,255);
+    analogWrite(TX,1023);
+  } else if (bars == 0) {
+    Serial.println("Wifi Signal is not available: 0");
+    //Set the RGBLED to show RED only
+//    rgbLed.writeRGB(255, 0, 0);
+    analogWrite(GPIO2, 1023);
+    analogWrite(RX,1023);
+    analogWrite(TX,1023);
+  }
+  //    }
+  //    previousQuality = quality;
+  //  }
 
-//////////////////////////////////////////
+  //////////////////////////////////////////
   Serial.println("Wait 3s before next round...");
   delay(3000);
 }
@@ -230,10 +249,10 @@ void loop() {
 //  #endif
 //  analogWrite(redPin, red);
 //  analogWrite(greenPin, green);
-//  analogWrite(bluePin, blue);  
+//  analogWrite(bluePin, blue);
 //}
 
-int getBarsSignal(long rssi){
+int getBarsSignal(long rssi) {
   // 5. High quality: 90% ~= -55db
   // 4. Good quality: 75% ~= -65db
   // 3. Medium quality: 50% ~= -75db
@@ -242,7 +261,7 @@ int getBarsSignal(long rssi){
   // 0. No signal
 
   int bars = 0;
-  if (WiFi.status() == WL_CONNECTED){
+  if (WiFi.status() == WL_CONNECTED) {
     if (rssi > -55) {
       bars = 5;
     } else if (rssi < -55 & rssi > -65) {
@@ -255,7 +274,7 @@ int getBarsSignal(long rssi){
       bars = 1;
     } else {
       bars = 0;
-    }  
+    }
   }
 
   return bars;
@@ -278,14 +297,6 @@ int getBarsSignal(long rssi){
 //  return 2 * (dBm + 100);
 //}
 
-//void printRgbValues() {
-//  Serial.println("Requested RGB Values:");
-//  Serial.println("(r,g,b)=(" + String(rgbLed.redValue) + "," + String(rgbLed.greenValue) + "," + String(rgbLed.blueValue) + ")");
-//  Serial.println("Mapped RGB Values based on type (COMMON_ANODE or COMMON_CATHODE):");
-//  Serial.println("Mapped(r,g,b)=(" + String(rgbLed.redMappedValue) + "," + String(rgbLed.greenMappedValue) + "," + String(rgbLed.blueMappedValue) + ")");
-//  Serial.println("------------------------------");
-//}
-
 //void fade(int pin) {
 //  for (int u = 0; u < 1024; u++) {
 //    analogWrite(pin, 1023 - u);
@@ -294,40 +305,5 @@ int getBarsSignal(long rssi){
 //  for (int u = 0; u < 1024; u++) {
 //    analogWrite(pin, u);
 //    delay(1);
-//  }
-//}
-
-//void setWifiSignlaLed(int signalBar){
-//  switch(signalBar){
-//    case 0:
-//    analogWrite(GPIO2, 255);
-////    analogWrite(RX,1023);
-//    analogWrite(TX,1023);
-//    break;
-//    case 1:
-//    analogWrite(GPIO2, 1023);
-////    analogWrite(RX,255);
-//    analogWrite(TX, 255);
-//    break;
-//    case 2:
-//    analogWrite(GPIO2, 255);
-////    analogWrite(RX, 1023);
-//    analogWrite(TX, 1023);
-//    break;
-//    case 3:
-//    analogWrite(GPIO2, 1023);
-////    analogWrite(RX, 1023);
-//    analogWrite(TX, 255);
-//    break;
-//    case 4:
-//    analogWrite(GPIO2, 255);
-////    analogWrite(RX, 1023);
-//    analogWrite(TX, 1023);
-//    break;
-//    case 5:
-//    analogWrite(GPIO2, 1023);
-////    analogWrite(RX,1023);
-//    analogWrite(TX,255);
-//    break;
 //  }
 //}
